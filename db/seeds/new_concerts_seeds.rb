7d0a80ffeeaafd061e0479525b8eed2def366b69
Catalina Galan <catalinagalanvargas@gmail.com>
2023-12-22 19:47:05 -0500

    seeded new artists and concerts

--- db/seeds/new_concerts_seeds.rb
+++ db/seeds/new_concerts_seeds.rb
@@ -1,15 +1,15 @@
 puts "creating new artists..."
 
 artists = [{
-  name: 'Olivia Rodrigo',
-  banner_url: "https://www.dexerto.com/cdn-cgi/image/width=1920,quality=75,format=auto/https://editors.dexerto.com/wp-content/uploads/2023/06/27/fans-upset-olivia-rodrigo-guts-album-cover-1.jpg",
-  photo_url: "https://evenko.ca/_uploads/event/57045/splash.jpg?v=1694617788"},
-  {name: 'Machine Head',
-  banner_url: "https://www.worshipmetal.com/wp-content/uploads/2017/11/Machine-Head-banner.jpg",
-  photo_url: "https://evenko.ca/en/events/58004/machine-head/mtelus/02-06-2024"},
-  {name: 'Jacob Collier',
+  # name: 'Olivia Rodrigo',
+  # banner_url: "https://www.dexerto.com/cdn-cgi/image/width=1920,quality=75,format=auto/https://editors.dexerto.com/wp-content/uploads/2023/06/27/fans-upset-olivia-rodrigo-guts-album-cover-1.jpg",
+  # photo_url: "https://evenko.ca/_uploads/event/57045/splash.jpg?v=1694617788"},
+  # {name: 'Machine Head',
+  # banner_url: "https://www.worshipmetal.com/wp-content/uploads/2017/11/Machine-Head-banner.jpg",
+  # photo_url: "https://evenko.ca/en/events/58004/machine-head/mtelus/02-06-2024"},
+  name: 'Jacob Collier',
   banner_url: "https://repo.thewildcity.com/3017-jacob-collier-banner.jpg",
-  photo_url: "https://media.artistsintown.com/300x300/9650707.webp"},
+  photo_url: "https://media.npr.org/assets/img/2016/07/11/jacob-collier2-18a5864435ce79a0416b9095b82d34f70f2dcdb4.jpg"},
   {name: 'Half Moon Run',
   banner_url: "https://thecbpshop.com/cdn/shop/files/HMR_BannerV0-06_a1c13a20-13f2-4839-9817-86e84bbfddf2_1600x.png?v=1677267615",
   photo_url: "https://s1.ticketm.net/dam/a/1d5/7c868895-42e3-4230-b86d-97d82ec971d5_EVENT_DETAIL_PAGE_16_9.jpg"},
@@ -59,10 +59,20 @@ artists.each do |band|
   artist.photo.attach(io: photo, filename: "band_photo.png", content_type: "image/png")
   puts "Creating concerts for #{band[:name]}"
 
-
-  puts "creating new concerts..."
+  shows.each do |show|
+    if show[:artist] == band
+      puts "creating #{artist.name} concert..."
+      concert = Concert.new(
+        artist_id: artist_id,
+        city: show[:city],
+        venue: show[:venue],
+        date: show[:date]
+      )
+      concert.save!
+      puts "Created new concert with id #{concert.id} for #{band[:name]}"
+    end
+  end
 end
-
 # concert = Concert.new
 # concert.city = "Montreal"
 # concert.venue = "Centre Bell"
